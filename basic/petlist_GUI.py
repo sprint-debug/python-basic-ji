121cacb540bc6ffba0a30bb80e7858715ae767a4
injji <metamong316@gmail.com>
2023-07-17 10:21:00 +0900

    feat: gui

---
+++ basic/petlist_GUI.py
@@ -0,0 +1,95 @@
+from tkinter import Tk, ttk, Label, Button, Text, END
+
+petstores = [
+            {"item": "Persian Adult Female", "price": 93.5},
+            {"item": "Bulldog Male Adult", "price": 18.5},
+            {"item": "Amazon Parrot Adult Male", "price": 193.5},
+            {"item": "Koi Spotted", "price": 18.5}
+        ]
+
+selected_index = 0
+
+def setTreeItems():
+    # Delete all rows on treePetstores
+    treePetstores.delete(*treePetstores.get_children())
+    for idx, petstore in enumerate(petstores): 
+        item = petstore['item']
+        price = petstore['price']
+        treePetstores.insert("", END, iid=None, text=str(idx), values=[item, price])   
+
+def petstores_selected(event):
+    global selected_index
+    for item in treePetstores.selection():
+        selected_index = int(treePetstores.item(item, "text"))
+    petstore = petstores[selected_index]
+    item = petstore['item']
+    price = str(petstore['price'])
+    text_Item.delete('1.0', END)
+    text_Item.insert(END, item)
+    text_Price.delete('1.0', END)
+    text_Price.insert(END, price)
+
+def insert_content():
+    item = text_Item.get('1.0', END)
+    price = float(text_Price.get('1.0', END))
+    petstore = { 'item': item, 'price': price }
+    petstores.append(petstore)
+    setTreeItems()
+
+def update_content():
+    global selected_index
+    item = text_Item.get('1.0', END)
+    price = float(text_Price.get('1.0', END))
+    selectedItem = petstores[selected_index]
+    selectedItem['item'] = item
+    selectedItem['price'] = price
+    setTreeItems()
+
+def delete_content():
+    global selected_index
+    petstores.pop(selected_index)
+    setTreeItems()
+
+window = Tk()
+window.title("Pet List Management")
+window.geometry("600x600")
+window.resizable(0,0)
+title = "Pet List Manager"
+lbl_title = Label(window, text=title, font=("helvetica",20))
+lbl_title.pack(padx=5, pady=15)
+
+# Display pet list on treePetstores
+treePetstores = ttk.Treeview(window)
+treePetstores['columns']=('item', 'price')
+treePetstores.column('#0', width=50)
+treePetstores.column('item', width=200)
+treePetstores.column('price', width=150)
+treePetstores.heading('#0', text='No')
+treePetstores.heading('item', text='Item')
+treePetstores.heading('price', text='Price')
+treePetstores.place(x=100, y=100, width=400, height=250)
+# Bind select event to petstores_selected
+treePetstores.bind('<<TreeviewSelect>>', petstores_selected)
+
+labelItem = Label(window, text='Item')
+labelItem.place(x=100, y=400, width=50, height=25)
+labelPrice = Label(window, text='Price')
+labelPrice.place(x=100, y=450, width=50, height=25)
+text_Item = Text(window, width=30, height=1)
+text_Item.place(x=200, y=400)
+text_Price = Text(window, width=30, height=1)
+text_Price.place(x=200, y=450)
+
+btn_Insert = Button(window, text='Insert', command=insert_content, font=("helvetica",14) )
+btn_Insert.place(x=100, y=500, width=100, height=30)
+
+btn_Update = Button(window, text='Update', command=update_content, font=("helvetica",14) )
+btn_Update.place(x=250, y=500, width=100, height=30)
+
+btn_Delete = Button(window, text='Delete', command=delete_content, font=("helvetica",14) )
+btn_Delete.place(x=400, y=500, width=100, height=30)
+
+# Initialize treePetstores
+setTreeItems()
+
+window.mainloop()
